c4fd7c950d7a7ea9264af7c1f77b44a1504e16ec
DougOsborn <dz@opensimian.com>
2015-09-23 16:57:44 -0600

    Update bootstrap.sh
    Added InstallLog Directory for log text files
    Added comments to clarify sections

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,26 +4,31 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
+# Update the repository package listings
 apt-get update
 
+# Install the base packages...  pipe the output to the InstallLogs directory
+mkdir InstallLogs
+
 echo " Installing git..    check git.txt for log"
-apt-get install -y git &> git.txt
+apt-get install -y git &> InstallLogs/git.txt
 
 echo " Installing mono..    check mono.txt for log"
-apt-get install -y mono-complete &> mono.txt
+apt-get install -y mono-complete &> InstallLogs/mono.txt
 
 echo " Installing monodev nunuit..    check monodev-nu.txt for log"
-apt-get install -y monodevelop-nunit &> monodev-nu.txt
+apt-get install -y monodevelop-nunit &> InstallLogs/monodev-nu.txt
 
 echo " Installing monodev version control..    check monodev-vc.txt for log"
-apt-get install -y monodevelop-versioncontrol &> monodev-vc.txt
+apt-get install -y monodevelop-versioncontrol &> InstallLogs/monodev-vc.txt
 
 echo " Installing nant..    check nant.txt for log"
-apt-get install -y nant &> nant.txt
+apt-get install -y nant &> InstallLogs/nant.txt
 
-cd /home/vagrant
+# clone the base OpenSimulator GIT repository
 git clone https://github.com/OpenSimian/opensimulator.git
 
+# try a build
 cd opensimulator
 ./runprebuild.sh
 nant

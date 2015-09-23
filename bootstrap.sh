945a20b76694423923e0bb31d985ed5d0ae3f384
DougOsborn <dz@opensimian.com>
2015-09-23 14:17:47 -0600

    Update bootstrap.sh
    Add  git to base package list to support local cloning

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
 apt-get update
-apt-get install -y mono-complete monodevelop-nunit monodevelop-versioncontrol > aptget.txt
+apt-get install -y git mono-complete monodevelop-nunit monodevelop-versioncontrol > aptget.txt
 
 mkdir /home/vagrant/OsMonoDev
 cd ~/OsMonoDev

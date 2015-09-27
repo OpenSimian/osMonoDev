73866fef6e3a28777c888fc0fbb990c90c5fbe77
DougOsborn <dz@opensimian.com>
2015-09-27 15:22:19 -0600

    Update loadSQL.sh

--- loadSQL.sh
+++ loadSQL.sh
@@ -36,7 +36,7 @@ service mysql start
 echo -e "\n--- Setting up our MySQL user and db ---\n"
 mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
 mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
-
+mysql -uroot -p$DBPASSWD -e "FLUSH PRIVILEGES;"
 
 /usr/sbin/update-rc.d mysql defaults
 

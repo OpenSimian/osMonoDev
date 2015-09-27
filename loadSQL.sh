c9d7be81805c117af3f5876321d8c94931688ae1
DougOsborn <dz@opensimian.com>
2015-09-26 21:47:44 -0600

    Update loadSQL.sh

--- loadSQL.sh
+++ loadSQL.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password 0p3ns1m1an'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 0p3ns1m1an'
+# Variables
+
+DBHOST=localhost
+DBNAME=opensim
+DBUSER=osUser
+DBPASSWD=0p3ns1m1an
+
+echo -e "\n--- Install MySQL specific packages and settings ---\n"
+echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
 
 # Install the base packages...  pipe the output to the InstallLogs directory
 mkdir InstallLogs
@@ -15,10 +23,14 @@ apt-get update  &> InstallLogs/aptUpdate.txt
 echo " Installing MySQL..    check mysql.txt for log"
 apt-get install -y mysql-server &> InstallLogs/mysql.txt
 
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+
 apt-cache show mysql-server
 
 update-rc.d mysql defaults
 
 service mysql start
 
-
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"

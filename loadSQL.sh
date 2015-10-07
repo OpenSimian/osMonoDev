06704fd44978c22d9b7b06160d08820d1047be3d
DougOsborn <dz@opensimian.com>
2015-10-07 00:16:39 -0600

    Update loadSQL.sh

--- loadSQL.sh
+++ loadSQL.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+sudo apt-key add mySQLgnuPGkey
+sudo echo "deb http://repo.mysql.com/apt/ubuntu/vivid mysql-5.6" >> /etc/apt/sources.list.d/mysql.list
+sudo dpkg -P mysql-server -y
+sudo apt-get install mysql-server -y
 
 
 /usr/sbin/mysqld --help --verbose

e917368d424117d30ad49a141e1984ee9800d008
DougOsborn <dz@opensimian.com>
2015-10-07 00:18:58 -0600

    Update loadSQL.sh

--- loadSQL.sh
+++ loadSQL.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
-sudo apt-key add mySQLgnuPGkey
-sudo echo "deb http://repo.mysql.com/apt/ubuntu/vivid mysql-5.6" >> /etc/apt/sources.list.d/mysql.list
-sudo dpkg -P mysql-server -y
-sudo apt-get install mysql-server -y
+apt-key add mySQLgnuPGkey
+echo "deb http://repo.mysql.com/apt/ubuntu/vivid mysql-5.6" >> /etc/apt/sources.list.d/mysql.list
+dpkg -P mysql-server -y
+
+echo " Installing MySQL..    check mysql.txt for log"
+apt-get install -y mysql-server
+
+apt-cache show mysql-server
+
+/sbin/iptables  -I INPUT -p tcp --dport 3306 -m state --state NEW,ESTABLISHED -j ACCEPT
+/sbin/iptables  -I OUTPUT -p tcp --sport 3306 -m state --state ESTABLISHED -j ACCEPT
+
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "bind-address = $DBBIND" >> /etc/mysql/my.cnf
+
+service mysql stop 
+
+service mysql start 
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
+mysql -uroot -p$DBPASSWD -e "FLUSH PRIVILEGES;"
+
 
 
 /usr/sbin/mysqld --help --verbose

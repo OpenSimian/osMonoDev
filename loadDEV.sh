4d5aa67bdf5f9f85d290c5143913a69faed566c3
DougOsborn <dz@opensimian.com>
2015-10-12 18:46:10 -0600

    Update loadDEV.sh

--- loadDEV.sh
+++ loadDEV.sh
@@ -51,3 +51,6 @@ nant
 
 # ensure access permissions
 chown -R vagrant /gitclones
+
+/sbin/iptables  -I INPUT -p udp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
+/sbin/iptables  -I OUTPUT -p udp --sport 9000 -m state --state ESTABLISHED -j ACCEPT
